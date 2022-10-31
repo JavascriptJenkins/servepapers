06ce47353ddacb8028eee0144622f99f2c83a815
humblemouse <javascriptjenkins@gmail.com>
2022-10-31 14:13:22 -0500

    add data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,45 @@
-DROP TABLE IF EXISTS cancel_train;
+DROP TABLE IF EXISTS process_data;
 
-CREATE SEQUENCE HIBERNATE_SEQUENCE START WITH 1 INCREMENT BY 1;
+--CREATE SEQUENCE HIBERNATE_SEQUENCE START WITH 1 INCREMENT BY 1;
 
-CREATE TABLE cancel_train (
+CREATE TABLE process_data (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   fname VARCHAR(250) NOT NULL,
+  mname VARCHAR(250) NOT NULL,
   lname VARCHAR(250) NOT NULL,
-  iscanceled BOOLEAN NOT NULL,
-  upvotes INT NOT NULL,
-  downvotes INT NOT NULL,
-  cancelstatus INT NOT NULL,
-  why VARCHAR(500) NOT NULL,
-  imageurl VARCHAR(250) NOT NULL,
+  age INT NOT NULL,
+  actioncounter1 INT NOT NULL,
+  actioncounter2 INT NOT NULL,
+  notes VARCHAR(1000) NOT NULL, -- Note, find out what is largest size for this notes section
+  address1 VARCHAR(500) NOT NULL,
+  address2 VARCHAR(500) NOT NULL,
+  race VARCHAR(500) NOT NULL,
+  city VARCHAR(500) NOT NULL,
+  state VARCHAR(500) NOT NULL,
+  zipcode VARCHAR(500) NOT NULL,
+  height VARCHAR(500) NOT NULL,
+  haircolor VARCHAR(250) NOT NULL,
   updatedtimestamp TIMESTAMP NOT NULL,
   createtimestamp TIMESTAMP NOT NULL
   );
 
+--CREATE TABLE cancel_train (
+--  id INT AUTO_INCREMENT  PRIMARY KEY,
+--  fname VARCHAR(250) NOT NULL,
+--  lname VARCHAR(250) NOT NULL,
+--  iscanceled BOOLEAN NOT NULL,
+--  upvotes INT NOT NULL,
+--  downvotes INT NOT NULL,
+--  cancelstatus INT NOT NULL,
+--  why VARCHAR(500) NOT NULL,
+--  imageurl VARCHAR(250) NOT NULL,
+--  updatedtimestamp TIMESTAMP NOT NULL,
+--  createtimestamp TIMESTAMP NOT NULL
+--  );
+--
+--
 
+-- token table is used for sending tokens to emails for login/reset password
 CREATE TABLE token (
     id INT AUTO_INCREMENT  PRIMARY KEY,
     token VARCHAR(250) NOT NULL,
@@ -25,6 +48,20 @@ CREATE TABLE token (
     updatedtimestamp DATE NOT NULL,
     createtimestamp DATE NOT NULL
 );
+
+-- systemuser is a user of the system
+CREATE TABLE systemuser (
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    username VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    roles VARCHAR(250) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    isuseractive INT NOT NULL,
+    updatedtimestamp DATE NOT NULL,
+    createtimestamp DATE NOT NULL
+);
+
 
 --   INSERT INTO cancel_train (fname,
 --                            lname,
